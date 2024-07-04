02ea98af7cd5412247906d5d7e35d45a1c397d76
DanielSchmerber <153175145+DanielSchmerber@users.noreply.github.com>
2024-07-04 13:34:54 +0200

    Update index.js
    Test Pipeline for presentation

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-    res.send(complexFunction("Hello ", "from Express"))
+    res.send(complexFunction("Hello ", "from Express!"))
 })
 
 
